c13a2f72131d7cf173b69a8308a368e8096ecda8
Shubham <105495246+ShubhamDot@users.noreply.github.com>
2022-09-03 14:24:52 +0000

    Update linklist.c

--- C,C++/linklist.c
+++ C,C++/linklist.c
@@ -1,3 +1,71 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node* next;
+}*head,*temp;
+
+struct node *head = NULL;
+struct node *temp = NULL;
+
+void displayll(){
+    
+    if (head == NULL){
+        printf("empty list\n");
+        return;
+    }
+    else{
+        temp = head;
+        while(temp!= NULL){
+            printf("->[%d]", temp->data);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
+void insert_at_beg(){
+    struct node* new = (struct node*) malloc(sizeof(struct node));
+    printf("Enter the data for the node.\n");
+    scanf("%d",&new->data);
+    
+    if(head == NULL){
+        head = new;
+    }
+    else{
+        
+        new->next = head;
+        head = new;
+    }
+    
+}
+
+
+void insert_at_node(){
+    struct node* new = (struct node*) malloc(sizeof(struct node));
+    printf("Enter the data for the node.\n");
+    scanf("%d",&new->data);
+    displayll();
+    printf("Enter node\n");
+    int pos;
+    scanf("%d", &pos);
+    if(head == NULL){
+        head = new;
+    }
+    else{
+        temp = head;
+        while(temp->data != pos){
+            temp = temp->next;
+            if(temp == NULL){
+                return;
+            }
+        }
+        new->next = temp->next;
+        temp->next = new;
+    }
+}
 int main(){
     
     
