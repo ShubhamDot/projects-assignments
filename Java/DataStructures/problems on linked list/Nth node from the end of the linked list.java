64fa2311fa1a9fb62896c85a30ca22fbd15700ed
Shubham <105495246+ShubhamDot@users.noreply.github.com>
2022-10-20 17:13:05 +0000

    Update and rename Java/DataStructures/Nth node from the end of the linked list.java to Java/DataStructures/problems on linked list/Nth node from the end of the linked list.java

--- Java/DataStructures/problems on linked list/Nth node from the end of the linked list.java
+++ Java/DataStructures/problems on linked list/Nth node from the end of the linked list.java
@@ -1,7 +1,7 @@
 class LinkedList {
     Node head; // head of the list
  
-    /* Linked List node */
+    
     class Node {
         int data;
         Node next;
@@ -12,8 +12,7 @@ class LinkedList {
         }
     }
  
-    /* Function to get the
-      Nth node from end of list */
+    
     void printNthFromLast(int N)
     {
         Node main_ptr = head;
@@ -52,17 +51,16 @@ class LinkedList {
         }
     }
  
-    /* Inserts a new Node at front of the list. */
+    
     public void push(int new_data)
     {
-        /* 1 & 2: Allocate the Node &
-                  Put in the data*/
+        
         Node new_node = new Node(new_data);
  
-        /* 3. Make next of new Node as head */
+        
         new_node.next = head;
  
-        /* 4. Move the head to point to new Node */
+        
         head = new_node;
     }
  
